69e5f8d84926faa1d5c052b00b87324b03c34fda
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-09-25 18:05:14 +0100

    Adding displayName

--- components/sticky/index.js
+++ components/sticky/index.js
@@ -3,29 +3,45 @@
  * Component using IntersectionObserver for scrollytelling
  */
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Observer from 'react-scroll-percentage';
 import './styles.scss';
 
-const Sticky = ({ article, graphic }) => (
-  <Observer>
-    {({ percentage, inView }) => (
-      <div className="sticky__container">
-        <article className="sticky__text">
-          {article({ percentage, inView })}
-        </article>
-        <figure className="sticky__figure">
-          {graphic({ percentage, inView })}
-        </figure>
-      </div>
-    )}
-  </Observer>
-);
+class Sticky extends PureComponent {
+  static displayName = 'GSticky';
+
+  state = {
+    current: null,
+  };
+
+  updateGraphic = (label) => {
+    this.setState({ current: label });
+  };
+
+  render() {
+    const { article, graphic } = this.props;
+    const { current } = this.state;
+    return (
+      <Observer>
+        {({ percentage, inView }) => (
+          <div className="sticky__container">
+            <article className="sticky__text">
+              {article({ percentage, inView, updateGraphic: this.updateGraphic })}
+            </article>
+            <figure className="sticky__figure">
+              {graphic({ percentage, inView, current })}
+            </figure>
+          </div>
+        )}
+      </Observer>
+    );
+  }
+}
 
 Sticky.propTypes = {
-  article: PropTypes.node.isRequired,
-  graphic: PropTypes.node.isRequired,
+  article: PropTypes.func.isRequired,
+  graphic: PropTypes.func.isRequired,
 };
 
 export default Sticky;
