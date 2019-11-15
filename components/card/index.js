236262706dc5137e7db47cc226291728bd438f42
Ændrew Rininsland <andrew.rininsland@ft.com>
2019-11-15 23:18:58 +0000

    Adds prop-types

--- components/card/index.js
+++ components/card/index.js
@@ -5,10 +5,19 @@
 
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import './styles.scss';
 
-const Card = ({ children, className, ...props }) => (
-  <div className={classNames(className, 'g-card')}>{children}</div>
-);
+const Card = ({ children, className, ...props }) =>
+  children && <div className={classNames(className, 'g-card')}>{children}</div>;
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+Card.defaultProps = {
+  className: null,
+};
 
 export default Card;
