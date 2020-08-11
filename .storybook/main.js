e008a0920d32af01ed8ba30d4f277debf9c1ec61
Ændrew Rininsland <andrew.rininsland@ft.com>
2020-08-11 16:33:07 +0100

    Upgrading to Storybook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 process.env.SASS_PATH="node_modules:node_modules/@financial-times"
 
 module.exports = {
-  stories: ['../src/**/*.stories.(js|mdx)'],
+  stories: ['../src/**/*.stories.mdx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
