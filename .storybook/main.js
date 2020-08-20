b6c396ae078067a0dd01b48e9b7811a1cd30ff53
Ændrew Rininsland <andrew.rininsland@ft.com>
2020-08-20 21:26:12 +0100

    Adding custom resolver

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,4 +10,18 @@ module.exports = {
     '@storybook/addon-knobs',
     '@storybook/addon-docs'
   ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need below:
+
+    // Resolve "browser" field in package.json (Origami)
+    config.resolve.aliasFields = ['browser'];
+    config.resolve.mainFields = ['main', 'browser'];
+
+    // Return the altered config
+    return config;
+  },
 };
