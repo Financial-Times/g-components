3db23974cee9731f8bc336de59cfc0d8413fa4ee
Cale Tilford <cale.tilford@ft.com>
2021-02-17 13:18:59 +0000

    Add offset prop, new stories and documentation

--- src/sticky-header/index.js
+++ src/sticky-header/index.js
@@ -1,46 +1,43 @@
 import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Context } from '../article-layout';
 import useScrollPosition from './useScrollPosition';
 import './styles.scss';
 
-const StickyHeader = ({ children, containerRef }) => {
+const StickyHeader = ({ children, containerRef, offset }) => {
   const [isSticky, setSticky] = useState(false);
   const [heightOfChildren, setHeightOfChildren] = useState(0);
   const [scrollDirection, setScrollDirection] = useState('down');
-  const ref = useRef(null);
+  const stickyRef = useRef(null);
   const childrenRef = useRef(null);
 
   useScrollPosition(({ prevPos, currPos }) => {
-    if (ref.current && containerRef.current) {
-      const { top: stickyTop, height: stickyHeight } = ref.current.getBoundingClientRect();
+    if (stickyRef.current && containerRef.current && childrenRef.current) {
       const { bottom: containerBottom } = containerRef.current.getBoundingClientRect();
-      const isSticky = stickyTop <= 0 && containerBottom - stickyHeight >= 0;
+      const { top: stickyTop, height: stickyHeight } = stickyRef.current.getBoundingClientRect();
+      const { height: heightOfChildren } = childrenRef.current.getBoundingClientRect();
 
-      if (childrenRef.current) {
-        const { height } = childrenRef.current.getBoundingClientRect();
-        setHeightOfChildren(height);
-      } else {
-        setHeightOfChildren(stickyHeight);
-      }
+      const isSticky = stickyTop <= offset && containerBottom - stickyHeight - offset >= 0;
 
+      setHeightOfChildren(heightOfChildren);
       setSticky(isSticky);
       setScrollDirection(currPos.y > prevPos.y ? 'up' : 'down');
     }
   }, []);
 
-  const wrapperClasses = classNames('sticky-header', isSticky && 'sticky-header--fixed');
+  const stickyHeaderClasses = classNames('sticky-header', isSticky && 'sticky-header--fixed');
 
   const childrenIsFunction = typeof children === 'function';
 
   return (
     <div
-      className={wrapperClasses}
-      ref={ref}
+      className={stickyHeaderClasses}
+      ref={stickyRef}
       style={{ minHeight: isSticky ? heightOfChildren : 'auto' }}
     >
-      {childrenIsFunction ? children({ isSticky, scrollDirection, ref: childrenRef }) : children}
+      <div className="sticky-header__children-wrapper" ref={childrenRef} style={{ top: offset }}>
+        {childrenIsFunction ? children({ isSticky, scrollDirection }) : children}
+      </div>
     </div>
   );
 };
@@ -51,6 +48,11 @@ StickyHeader.propTypes = {
     PropTypes.func,
     PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
   ]).isRequired,
+  offset: PropTypes.number,
+};
+
+StickyHeader.defaultProps = {
+  offset: 0,
 };
 
 export default StickyHeader;
