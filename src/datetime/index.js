8d0f0288ee0b58d5a8a341a54b2a1dc1a9a0fbd5
Joanna Kao <joannaskao@gmail.com>
2021-11-26 16:17:24 +0000

    Remove console log

--- src/datetime/index.js
+++ src/datetime/index.js
@@ -6,7 +6,7 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import ODate from '@financial-times/o-date';
 import ftDateFormat from '@financial-times/ft-date-format';
-console.log(ODate);
+
 const DateTime = ({ datestamp }) => {
   const elRef = useRef(null);
   const dateRef = useRef(null);
