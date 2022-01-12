b7570e68b6c2cd5d18d6c09612e4927474b2a439
Ændra Rininsland <hello@aendra.com>
2022-01-12 13:45:13 +0000

    Adds build process

--- build.mjs
+++ build.mjs
@@ -1,16 +1,20 @@
 import { sassPlugin } from 'esbuild-sass-plugin';
 import { build } from 'esbuild';
 
-await build({
-  loader: {
-    '.js': 'jsx',
-  },
-  entryPoints: ['src/index.js'],
-  bundle: true,
-  outfile: 'build/g-components.js',
-  plugins: [
-    sassPlugin({
-      loadPaths: ['node_modules', 'node_modules/@financial-times'],
-    }),
-  ],
-}).catch(() => process.exit(1));
+for (const format of ['cjs', 'esm']) {
+  await build({
+    minify: format === 'cjs',
+    loader: {
+      '.js': 'jsx',
+    },
+    entryPoints: ['src/index.js'],
+    bundle: true,
+    format,
+    outfile: format === 'cjs' ? 'build/g-components.js' : 'build/g-components.esm.js',
+    plugins: [
+      sassPlugin({
+        loadPaths: ['node_modules', 'node_modules/@financial-times'],
+      }),
+    ],
+  }).catch(() => process.exit(1));
+}
