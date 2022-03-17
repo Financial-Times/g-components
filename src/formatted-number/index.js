63660c4911efc82a5be64ceba69d0f6a4a71fd66
emma.lewis <emma.lewis@ft.com>
2022-03-17 17:42:07 +0000

    Remove useeffect hook

--- src/formatted-number/index.js
+++ src/formatted-number/index.js
@@ -12,66 +12,64 @@ const FormattedNumber = ({ x, spellBelowTen }) => {
   let shortSuffix;
   let accessibleSuffix;
 
-  useEffect(() => {
-    const num = Number(x); // Ensure typed as Number
+  const num = Number(x); // Ensure typed as Number
 
-    if (Number.isNaN(num)) {
-      setNumber(null);
-      return;
-    }
+  if (Number.isNaN(num)) {
+    setNumber(null);
+    return;
+  }
 
-    // Find if negative
-    const n_sgn = num < 0 ? '-' : '';
+  // Find if negative
+  const nSign = num < 0 ? '-' : '';
 
-    // Set as absolute value
-    const n_abs = Math.abs(num);
+  // Set as absolute value
+  const nAbs = Math.abs(num);
 
-    // Round and convert to string
-    let n;
+  // Round and convert to string
+  let n;
 
-    if (n_abs < 1e3) {
-      shortSuffix = '';
-      accessibleSuffix = '';
-      n = trimNumber(round(n_abs));
-    } else if (n_abs < 1e6) {
-      shortSuffix = 'k';
-      accessibleSuffix = 'thousand';
-      n = trimNumber(round(n_abs / 1e3));
-    } else if (n_abs < 1e9) {
-      shortSuffix = 'mn';
-      accessibleSuffix = 'million';
-      n = trimNumber(round(n_abs / 1e6));
-    } else if (n_abs < 1e12) {
-      shortSuffix = 'bn';
-      accessibleSuffix = 'billion';
-      n = trimNumber(round(n_abs / 1e9));
-    } else if (n_abs < 1e15) {
-      shortSuffix = 'tn';
-      accessibleSuffix = 'trillion';
-      n = trimNumber(round(n_abs / 1e12));
-    } else if (n_abs < 1e18) {
-      shortSuffix = ' quadrillion';
-      accessibleSuffix = 'quadrillion';
-      n = trimNumber(round(n_abs / 1e15));
-    } else {
-      shortSuffix = '';
-      accessibleSuffix = '';
-      n = new Intl.NumberFormat('en-GB', {
-        maximumSignificantDigits: 3,
-        notation: 'scientific',
-      }).format(round(x));
-    }
+  if (nAbs < 1e3) {
+    shortSuffix = '';
+    accessibleSuffix = '';
+    n = trimNumber(round(nAbs));
+  } else if (nAbs < 1e6) {
+    shortSuffix = 'k';
+    accessibleSuffix = 'thousand';
+    n = trimNumber(round(nAbs / 1e3));
+  } else if (nAbs < 1e9) {
+    shortSuffix = 'mn';
+    accessibleSuffix = 'million';
+    n = trimNumber(round(nAbs / 1e6));
+  } else if (nAbs < 1e12) {
+    shortSuffix = 'bn';
+    accessibleSuffix = 'billion';
+    n = trimNumber(round(nAbs / 1e9));
+  } else if (nAbs < 1e15) {
+    shortSuffix = 'tn';
+    accessibleSuffix = 'trillion';
+    n = trimNumber(round(nAbs / 1e12));
+  } else if (nAbs < 1e18) {
+    shortSuffix = ' quadrillion';
+    accessibleSuffix = 'quadrillion';
+    n = trimNumber(round(nAbs / 1e15));
+  } else {
+    shortSuffix = '';
+    accessibleSuffix = '';
+    n = new Intl.NumberFormat('en-GB', {
+      maximumSignificantDigits: 3,
+      notation: 'scientific',
+    }).format(round(x));
+  }
 
-    //  Add `-` back in if negative
-    const signed_number = n_sgn + n;
+  //  Add `-` back in if negative
+  const signedNumber = nSign + n;
 
-    if (n_abs < 10 && spellBelowTen) {
-      number = spellOutNumber(signed_number);
-      return;
-    }
+  if (nAbs < 10 && spellBelowTen) {
+    number = spellOutNumber(signedNumber);
+    return;
+  }
 
-    number = signed_number;
-  }, [x]);
+  number = signedNumber;
 
   return number ? (
     <span>
