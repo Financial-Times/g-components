28c7fc9abd7a80ac1b48aea99da9bbb0f086605d
Joanna Kao <joannaskao@gmail.com>
2023-03-23 17:14:34 +0000

    Update FlourishEmbed story

--- src/flourish-embed/index.stories.jsx
+++ src/flourish-embed/index.stories.jsx
@@ -2,9 +2,21 @@ import FlourishEmbed from './';
 
 export default {
   title: 'Core/FlourishEmbed',
+  component: FlourishEmbed,
+
+  parameters: {
+    chromatic: {
+      disable: true,
+    },
+  },
 };
 
 export const Example = {
-  render: () => <div>Basic Flourish embed</div>,
+  render: () => (
+    <FlourishEmbed
+      alt="Timeline of government responses"
+      url="https://public.flourish.studio/visualisation/7730043/"
+    />
+  ),
   name: 'Example',
 };
